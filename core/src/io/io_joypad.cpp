32f420e3174f048b9d69d449e87b8ea9e36ab100
Matt Seabold <matt.seabold@gmail.com>
2020-03-25 00:27:40 -0400

    Make sure Joypad stays low when nothing is selcted

--- core/src/io/io_joypad.cpp
+++ core/src/io/io_joypad.cpp
@@ -14,16 +14,17 @@ void IoJoypad::writeAddr(uint16_t addr, uint8_t val) {
 }
 
 uint8_t IoJoypad::readAddr(uint16_t addr) {
-    uint8_t val = mSelection;
+    uint8_t val = mSelection | IOREG_P1_IN_MASK;
     if(addr == IOREG_P1) {
         // Remember: all these pins are active low!
         if(!(mSelection & IOREG_P1_OUT_P14)) {
-            val |= (mMask & JOYPAD_BMASK_P14_MASK) ^ JOYPAD_BMASK_P14_MASK;
+            val ^= mMask & JOYPAD_BMASK_P14_MASK;
         }
 
         if(!(mSelection & IOREG_P1_OUT_P15)) {
-            val |= ((mMask & JOYPAD_BMASK_P15_MASK) ^ JOYPAD_BMASK_P15_MASK) >> JOYPAD_BMASK_P15_SHIFT;
+            val ^= (mMask & JOYPAD_BMASK_P15_MASK) >> JOYPAD_BMASK_P15_SHIFT;
         }
+
         return val;
     }
     return 0xff;
