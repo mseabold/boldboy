2de664a8b631b72b96e9f0d9f53898b0fb9e2ee3
Matt <matt.seabold@gmail.com>
2020-10-25 22:56:25 -0400

    oops

--- app/qt/BoldboyQT/main.cpp
+++ app/qt/BoldboyQT/main.cpp
@@ -7,7 +7,7 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     BoldboyQT w;
-    if(argc >- 2) {
+    if(argc >= 2) {
         w.loadRom((const char *)argv[1]);
     }
     w.show();
